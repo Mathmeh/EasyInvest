a514ddbce008200362261a0bdf617547f60d63db
DenverDen <denverden@mail.ru>
2024-07-10 02:59:43 +0300

    feat: add button and fragment asset

---
+++ app/src/main/java/com/example/easyinvest/presentation/asset/FragmentAsset.kt
@@ -0,0 +1,29 @@
+package com.example.easyinvest.presentation.asset
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.easyinvest.databinding.FragmentAssetBinding
+
+class FragmentAsset : Fragment() {
+    private var _binding: FragmentAssetBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAssetBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
