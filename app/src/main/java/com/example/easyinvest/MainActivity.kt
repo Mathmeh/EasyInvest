96c8284d53ccf76cfacbfcd49fc0e442bad83d25
DenverDen <denverden@mail.ru>
2024-07-10 21:41:23 +0300

    feat: add navigation fragment

--- app/src/main/java/com/example/easyinvest/MainActivity.kt
+++ app/src/main/java/com/example/easyinvest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.easyinvest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.example.easyinvest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            val navController = findNavController(R.id.fragmentContainerView)
+            when (item.itemId) {
+                R.id.navigation_portfolio -> {
+                    navController.navigate(R.id.fragmentPortfolio)
+                    true
+                }
+                R.id.navigation_asset -> {
+                    navController.navigate(R.id.fragmentAsset)
+                    true
+                }
+                R.id.navigation_settings -> {
+                    navController.navigate(R.id.fragmentSettings)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
